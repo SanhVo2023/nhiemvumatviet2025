1dfa5816315a5607514c9d6c1467167913e26c81
Your Name <thachsanhoracle@gmail.com>
2025-04-29 17:56:20 +0700

    Updated configs for Netlify static site deployment

--- next.config.mjs
+++ next.config.mjs
@@ -6,9 +6,11 @@ const nextConfig = {
   typescript: {
     ignoreBuildErrors: true,
   },
+  output: 'export',
   images: {
     unoptimized: true,
   },
+  trailingSlash: true,
 }
 
 export default nextConfig
